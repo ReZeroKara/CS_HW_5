d972b7923168fdcdafcd4496d73c338b2c898166
ReZero <ghostofpast@icloud.com>
2023-04-13 18:24:02 +0300

    complete tasks

---
+++ task001_Arr/Program.cs
@@ -0,0 +1,37 @@
+﻿//Программа, которая покажет количество чётных чисел в массиве.
+
+Console.Write("Сколько чисел будет в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+fillArray(numbers);
+Console.WriteLine("Массив: ");
+printArray(numbers);
+
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+if (numbers[i] % 2 == 0)
+count++;
+
+Console.WriteLine($"Всего {numbers.Length} чисел, {count} из них чётные");
+
+void fillArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+
+void printArray(int[] numbers)
+{
+    Console.Write("[ ");
+
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+
+    Console.Write("]");
+    Console.WriteLine();
+}
---
+++ task002_Arr2/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Write("Сколько чисел будет в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+fillArray(numbers);
+Console.WriteLine("Мссив: ");
+printArray(numbers);
+
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i += 2)
+    sum = sum + numbers[i];
+
+    Console.WriteLine($"Всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+void fillArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+
+void printArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
